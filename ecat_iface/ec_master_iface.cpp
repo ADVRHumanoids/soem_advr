46f174af20d02dd3b91d8686187d97a562568c42
Alessio Margan <alessio.margan@iit.it>
2015-03-10 18:28:27 +0100

    master sync offset 400000

--- ecat_iface/ec_master_iface.cpp
+++ ecat_iface/ec_master_iface.cpp
@@ -59,7 +59,7 @@ static int ecat_cycle(void) {
 static void ec_sync(const int64_t reftime, const uint64_t cycletime , int64_t* offsettime)
 {
     /* master sync offset with ec_DCtime */
-    static const uint32_t sync_point_ns = 300000; //500000;
+    static const uint32_t sync_point_ns = 400000; //300000; //500000;
     static int64_t integral = 0;
     int64_t delta;
 
